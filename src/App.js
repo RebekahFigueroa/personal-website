2868b84c8517012e4e62a043da736aff3c601ed6
rebekahfigueroa <figueroarebekah@gmail.com>
2023-03-21 10:19:29 -0500

    added the react routing for each page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import { Box } from "@mui/material";
 import React from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import Blog from "./Blog/Blog";
 import ContactMe from "./ContactMe/ContatctMe";
 import Experience from "./Experience/Experience";
-import Introduction from "./Introduction/Introduction";
+import Home from "./Home/Home";
 import NavBar from "./NavBar/NavBar";
 import Portfolio from "./Portfolio/Portfolio";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <NavBar />
       <Box sx={{ height: "calc(100vh - 64px)", marginTop: "64px" }}>
-        <Introduction />
-        <Experience />
-        <Portfolio />
-        <Blog />
-        <ContactMe />
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/experience">
+            <Experience />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route path="/contact-me">
+            <ContactMe />
+          </Route>
+          <Redirect from="/" to="/home" />
+        </Switch>
       </Box>
-    </div>
+    </BrowserRouter>
   );
 }
 

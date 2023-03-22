371836b661bb0fea87da63fda64eecdebca2a5a2
rebekahfigueroa <figueroarebekah@gmail.com>
2023-03-22 16:01:07 -0500

    updated blog structure

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -1,7 +1,39 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
+import BlogCards from "./BlogCards/BlogCards";
 
 function Blog() {
-  return <div>Blog</div>;
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "3rem",
+        }}
+      >
+        <Box>
+          <Typography gutterBottom variant="h1" component="div">
+            Technical Blog @ Medium
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            width: "80%",
+            display: "flex",
+            justifyContent: "space-evenly",
+            mb: 4,
+          }}
+        >
+          <BlogCards />
+          <BlogCards />
+          <BlogCards />
+        </Box>
+      </Box>
+    </>
+  );
 }
 
 export default Blog;

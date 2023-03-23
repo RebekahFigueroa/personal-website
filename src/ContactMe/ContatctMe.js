ba262436f2e7e3de69c7b797f2433e955af0413c
rebekahfigueroa <figueroarebekah@gmail.com>
2023-03-23 09:49:33 -0500

    created contact me page

--- src/ContactMe/ContatctMe.js
+++ src/ContactMe/ContatctMe.js
@@ -1,7 +1,57 @@
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
 
 function ContactMe() {
-  return <div>ContactMe</div>;
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "3rem",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "3rem",
+          }}
+        >
+          <Typography gutterBottom variant="h1" component="div">
+            Want to get in touch?
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "2rem",
+              marginRight: "32rem",
+              marginLeft: "32rem",
+              color: "text.secondary",
+            }}
+          >
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Vestibulum fringilla erat ut tellus accumsan tempus. Proin
+              faucibus, purus et tristique malesuada, nulla risus varius ante,
+              quis ultricies lectus justo volutpat lorem.
+            </Typography>
+          </Box>
+          <Button variant="contained" sx={{ marginTop: "2rem" }}>
+            Contact Me
+          </Button>
+        </Box>
+        <Box></Box>
+      </Box>
+    </>
+  );
 }
 
 export default ContactMe;
